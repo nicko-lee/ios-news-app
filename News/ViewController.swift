e77667c06d0191402d3f638c16a1491d236b897d
Nicko Lee <nicko.lee@anz.com>
2020-04-02 21:53:35 +1100

    Initial project setup and first commit as baseline

---
+++ News/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  News
+//
+//  Created by Nicko Lee on 4/2/20.
+//  Copyright © 2020 Nicko Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
